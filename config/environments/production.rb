9d07dfb1b04181afd6a03766ed5372001c16e404
Guilherme Baufaker <baufaker@gmail.com>
2011-10-18 16:14:45 -0200

    mudei o default do mailer no production pro site

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,4 +58,5 @@ Cantucci::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
+  config.action_mailer.default_url_options = { :host => 'cantucci.heroku.com' }
 end
