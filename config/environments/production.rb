4f1b8ed4b02d68ef53999f49c57fe0dcc0f05082
Guilherme Baufaker <baufaker@gmail.com>
2011-10-18 15:58:12 -0200

    config.assets.initialize_on_precompile = false para funcionar no heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,5 @@ Cantucci::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'cantucci.heroku.com' }
+  config.assets.initialize_on_precompile = false
 end
