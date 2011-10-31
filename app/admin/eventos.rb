15fb20222715544252b6be577b43147c91923e0e
Guilherme Baufaker <baufaker@gmail.com>
2011-10-31 20:09:12 -0200

    Modifiquei a view de admn da agenda, que estava mostrando as tags de html

--- app/admin/eventos.rb
+++ app/admin/eventos.rb
@@ -1,4 +1,13 @@
 ActiveAdmin.register Evento do
   menu :label => "Agenda", :priority => 3
   
+  index do 
+    column "Nome do Evento", :title
+    column "Descricao", :description do |dsc| 
+      truncate(dsc.description).html_safe
+    end
+    column "Criado em", :created_at
+    default_actions
+  end
+  
 end
