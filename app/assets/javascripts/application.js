4ca8564aeceaa437b3d3faf937fd254718792c06
Guilherme Baufaker <baufaker@gmail.com>
2011-11-30 11:22:17 -0200

    konami code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,18 @@
 //= require scripts
 //= require jquery_ujs
 //= require_tree .
+
+// $(document).ready(function(){
+//  	$('body').keypress(
+// 		function(){alert('penis!');}
+// 	);
+// });
+
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";  
+$(document).keydown(function(e) {  
+  kkeys.push( e.keyCode );  
+  if ( kkeys.toString().indexOf( konami ) >= 0 ){  
+    $(document).unbind('keydown',arguments.callee);  
+    alert('PINTO DO IAGO!');
+  }  
+});
