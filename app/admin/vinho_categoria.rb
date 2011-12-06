2a1b6ff5cfbb7163429f6a108b3f84b394e5913f
Guilherme Baufaker <baufaker@gmail.com>
2011-12-06 19:23:17 -0200

    asdasdas

--- app/admin/vinho_categoria.rb
+++ app/admin/vinho_categoria.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register VinhoCategoria do
   
+  menu :label => "Categorias(vinhos)"
+  
+  index do 
+     column "Nome", :title
+     default_actions
+   end
+   
+   form do |f|
+     f.inputs "Categorias" do
+             f.input :title
+             f.input :foto, as: :file
+           end
+     f.buttons
+   end
 end
